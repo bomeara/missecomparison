44c006d393160de084ef4f527806e19a5d1cf198
Brian O'Meara <omeara.brian@gmail.com>
2019-03-22 17:25:57 -0400

    dealing with corner case with no hidden rates

--- PascalRaboskyRuns/R/functions.R
+++ PascalRaboskyRuns/R/functions.R
@@ -1,22 +1,33 @@
 #' Function to do a single MiSSE run
 #'
 #' @param dir Where the phylo object resides
+#' @param phy The phylo object
 #' @param nturnover How many turnover categories to use
 #' @param neps_same Whether to have same number of eps categories (TRUE) or just one (FALSE)
 
-DoSingleRun <- function(dir, nturnover=2, neps_same=TRUE, root_type="madfitz") {
+DoSingleRun <- function(dir, phy, nturnover=2, neps_same=TRUE, root_type="madfitz") {
 #	print(system(paste0("ls data/pascal_rabosky_dryad/trees/", dir)))
 #	print(paste0("data/pascal_rabosky_dryad/trees/", dir, "/", dir, ".tre"))
 #	phy <- NULL
-try(phy <- ape::read.tree(paste0("data/pascal_rabosky_dryad/trees/", dir, "/", dir, ".tre")))
+#try(phy <- ape::read.tree(paste0("data/pascal_rabosky_dryad/trees/", dir, "/", dir, ".tre")))
+	start_time <- Sys.time()
 	if(!is.null(phy)) {
 	#print(phy)
 	  #dir <- name(phy)
 		turnover <- sequence(nturnover)
-		eps <- ifelse(neps_same, turnover, rep(1, nturnover))
+		eps <- turnover
+		if(neps_same) {
+			eps <- rep(1, nturnover)
+		}
+		#eps <- ifelse(neps_same, turnover, rep(1, nturnover))
 		hisse_result <- hisse::MiSSE(phy, f=1, turnover=turnover, eps=eps, root.type=root_type)
 		hisse_recon <- hisse::MarginReconMiSSE(phy=phy, f=1,  hidden.states=nturnover, pars=hisse_result$solution, aic=hisse_result$AIC, root.type=root_type)
-		summary_df <- t(hisse_recon$rates.mat %*% t(hisse_recon$tip.mat[,-1]))
+		tip_mat_transformed <- hisse_recon$tip.mat[,-1]
+		if(max(tip_mat_transformed) == 0) {
+			tip_mat_transformed[,1] <- 1 #deal with misse bug of no weight if no hidden
+		}
+		summary_df <- t(hisse_recon$rates.mat %*% t(tip_mat_transformed))
+		summary_df <- data.frame(summary_df)
 		summary_df$treeName <- dir
 		summary_df$taxon_id_in_phy <- sequence(nrow(summary_df))
 		summary_df$tipName <- phy$tip.label
@@ -25,6 +36,7 @@ try(phy <- ape::read.tree(paste0("data/pascal_rabosky_dryad/trees/", dir, "/", d
 		summary_df$AIC <- hisse_result$AIC
 		summary_df$AICc <- hisse_result$AICc
 		summary_df$root_type <- hisse_result$root.type
+		summary_df$elapsed_time <- Sys.time() - start_time
 		return(list(hisse_result=hisse_result, hisse_recon=hisse_recon, summary_df=summary_df))
 	} else {
 		return(list(failure=dir))
--- PascalRaboskyRuns/R/plan.R
+++ PascalRaboskyRuns/R/plan.R
@@ -1,6 +1,6 @@
 tree_info <- read.csv(file=file_in("data/pascal_rabosky_dryad/tipRates_dryad/dataFiles/treeSummary.csv"),stringsAsFactors=FALSE)
-tree_names <- unique(tree_info$treeName)[1:3] #limit to 1:3 for trial
-tree_names <- paste0("evolvingRates_", sequence(4))
+tree_names <- unique(tree_info$treeName)
+tree_names <- tree_names[grepl("1$", tree_names)] # for speed, only take a tenth of the trees: those ending in a 1.
 trees <- list()
 for (i in seq_along(tree_names)) {
   trees[[i]] <- ape::read.tree(paste0("data/pascal_rabosky_dryad/trees/", tree_names[i], "/", tree_names[i], ".tre"))
@@ -13,6 +13,10 @@ names(trees) <- tree_names
 #    hisse_out = target(DoSingleRun(dir), transform = map(dir = !!tree_names))
 # )
 #
+# plan <- drake_plan(
+#    hisse_out = target(DoSingleRun(dir, nturnover=turnover_states, neps_same=neps_same_states, root_type=root_type_states), transform = cross(dir = !!tree_names, turnover_states=!!sequence(10), neps_same_states=c(TRUE), root_type_states=!!c('madfitz')))
+# )
+
 plan <- drake_plan(
-   hisse_out = target(DoSingleRun(dir, nturnover=turnover_states, neps_same=neps_same_states, root_type=root_type_states), transform = cross(dir = !!tree_names, turnover_states=!!sequence(10), neps_same_states=c(TRUE), root_type_states=!!c('madfitz')))
+   hisse_out = target(DoSingleRun(dir=tree_names[tree_index], phy=trees[[tree_index]], nturnover=turnover_states, neps_same=neps_same_states, root_type=root_type_states), transform = cross(tree_index=!!sequence(length(trees)), turnover_states=!!sequence(10), neps_same_states=c(TRUE), root_type_states=!!c('madfitz')))
 )
