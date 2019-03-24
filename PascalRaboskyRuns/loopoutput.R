84a4a1d917c3ba2e5eeae5f5ebd8b783a70a9ad1
Brian O'Meara <omeara.brian@gmail.com>
2019-03-24 13:18:18 -0400

    debugging loading script

--- PascalRaboskyRuns/loopoutput.R
+++ PascalRaboskyRuns/loopoutput.R
@@ -2,8 +2,10 @@
 # git config --global credential.helper cache
 # git config --global credential.helper 'cache --timeout=360000'
 
+library(drake)
 
 while (1<2) {
+  drake::loadd(drake::cached())
   result.df <- dplyr::bind_rows(lapply(drake::cached(), get))
   write.csv(result.df, file="result.csv")
   system(paste0('git commit -m"updating with ', length(drake::cached()), ' runs done" result.csv'))
