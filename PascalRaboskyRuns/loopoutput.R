4570c5c53db616a15b4ff3736b1eb22015087191
Brian O'Meara <omeara.brian@gmail.com>
2019-03-24 13:17:04 -0400

    looping script to run in background

---
+++ PascalRaboskyRuns/loopoutput.R
@@ -0,0 +1,12 @@
+# Not require password
+# git config --global credential.helper cache
+# git config --global credential.helper 'cache --timeout=360000'
+
+
+while (1<2) {
+  result.df <- dplyr::bind_rows(lapply(drake::cached(), get))
+  write.csv(result.df, file="result.csv")
+  system(paste0('git commit -m"updating with ', length(drake::cached()), ' runs done" result.csv'))
+  try(system("git push"))
+  Sys.sleep(60*60)
+}
