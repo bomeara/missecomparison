cd60d702f1e2a290b3cd84f1e78bb5644d31c0c5
Brian OMeara <omeara.brian@gmail.com>
2019-04-03 15:50:30 -0400

    uses parallel cores for recon

--- TitleRaboskyRuns/R/functions.R
+++ TitleRaboskyRuns/R/functions.R
@@ -22,7 +22,7 @@ DoSingleRun <- function(dir, phy, root_type="madfitz") {
 			nturnover <- length(unique(hisse_result_all[[model_index]]$turnover))
 			neps <- length(unique(hisse_result_all[[model_index]]$eps))
 
-			hisse_recon <- hisse::MarginReconMiSSE(phy=hisse_result_all[[model_index]]$phy, f=1,  hidden.states=nturnover, pars=hisse_result_all[[model_index]]$solution, aic=hisse_result_all[[model_index]]$AIC, root.type=root_type, get.tips.only=TRUE)
+			hisse_recon <- hisse::MarginReconMiSSE(phy=hisse_result_all[[model_index]]$phy, f=1,  hidden.states=nturnover, pars=hisse_result_all[[model_index]]$solution, aic=hisse_result_all[[model_index]]$AIC, root.type=root_type, get.tips.only=TRUE, n.cores=parallel::detectCores())
 			tip_mat_transformed <- hisse_recon$tip.mat[,-1]
 			if(max(tip_mat_transformed) == 0) {
 				tip_mat_transformed[,1] <- 1 #deal with misse bug of no weight if no hidden
