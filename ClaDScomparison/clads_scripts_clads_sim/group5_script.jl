404152c7b777c99dfa9313fcd662b46fdf997d2d
tncvasconcelos <thais.nogales@gmail.com>
2022-03-05 11:34:06 -0600

    clads runs

--- ClaDScomparison/clads_scripts_clads_sim/group5_script.jl
+++ ClaDScomparison/clads_scripts_clads_sim/group5_script.jl
@@ -3,125 +3,125 @@
 # mode: julia 
 	 using JLD2 
 # mode: julia 
-	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDSe_100_5_1.tre") 
+	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDS2tree_100_5_1.tre") 
 # mode: julia 
 	 output = infer_ClaDS(my_tree, print_state = 100) 
 # mode: julia 
-	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDSe_100_5_1_clads_results.Rdata") 
+	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDS2tree_100_5_1_clads_results.Rdata") 
 # mode: julia 
-	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDSe_100_5_2.tre") 
-# mode: julia 
-	 output = infer_ClaDS(my_tree, print_state = 100) 
-# mode: julia 
-	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDSe_100_5_2_clads_results.Rdata") 
-# mode: julia 
-	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDSe_100_5_3.tre") 
+	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDS2tree_100_5_2.tre") 
 # mode: julia 
 	 output = infer_ClaDS(my_tree, print_state = 100) 
 # mode: julia 
-	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDSe_100_5_3_clads_results.Rdata") 
+	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDS2tree_100_5_2_clads_results.Rdata") 
 # mode: julia 
-	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDSe_100_5_4.tre") 
-# mode: julia 
-	 output = infer_ClaDS(my_tree, print_state = 100) 
-# mode: julia 
-	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDSe_100_5_4_clads_results.Rdata") 
-# mode: julia 
-	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDSe_100_5_5.tre") 
+	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDS2tree_100_5_3.tre") 
 # mode: julia 
 	 output = infer_ClaDS(my_tree, print_state = 100) 
 # mode: julia 
-	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDSe_100_5_5_clads_results.Rdata") 
+	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDS2tree_100_5_3_clads_results.Rdata") 
 # mode: julia 
-	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDSe_100_6_1.tre") 
-# mode: julia 
-	 output = infer_ClaDS(my_tree, print_state = 100) 
-# mode: julia 
-	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDSe_100_6_1_clads_results.Rdata") 
-# mode: julia 
-	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDSe_100_6_2.tre") 
+	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDS2tree_100_5_4.tre") 
 # mode: julia 
 	 output = infer_ClaDS(my_tree, print_state = 100) 
 # mode: julia 
-	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDSe_100_6_2_clads_results.Rdata") 
+	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDS2tree_100_5_4_clads_results.Rdata") 
 # mode: julia 
-	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDSe_100_6_3.tre") 
-# mode: julia 
-	 output = infer_ClaDS(my_tree, print_state = 100) 
-# mode: julia 
-	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDSe_100_6_3_clads_results.Rdata") 
-# mode: julia 
-	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDSe_100_6_4.tre") 
+	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDS2tree_100_5_5.tre") 
 # mode: julia 
 	 output = infer_ClaDS(my_tree, print_state = 100) 
 # mode: julia 
-	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDSe_100_6_4_clads_results.Rdata") 
+	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDS2tree_100_5_5_clads_results.Rdata") 
 # mode: julia 
-	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDSe_100_6_5.tre") 
-# mode: julia 
-	 output = infer_ClaDS(my_tree, print_state = 100) 
-# mode: julia 
-	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDSe_100_6_5_clads_results.Rdata") 
-# mode: julia 
-	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDSe_100_7_1.tre") 
+	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDS2tree_100_6_1.tre") 
 # mode: julia 
 	 output = infer_ClaDS(my_tree, print_state = 100) 
 # mode: julia 
-	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDSe_100_7_1_clads_results.Rdata") 
+	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDS2tree_100_6_1_clads_results.Rdata") 
 # mode: julia 
-	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDSe_100_7_2.tre") 
-# mode: julia 
-	 output = infer_ClaDS(my_tree, print_state = 100) 
-# mode: julia 
-	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDSe_100_7_2_clads_results.Rdata") 
-# mode: julia 
-	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDSe_100_7_3.tre") 
+	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDS2tree_100_6_2.tre") 
 # mode: julia 
 	 output = infer_ClaDS(my_tree, print_state = 100) 
 # mode: julia 
-	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDSe_100_7_3_clads_results.Rdata") 
+	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDS2tree_100_6_2_clads_results.Rdata") 
 # mode: julia 
-	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDSe_100_7_4.tre") 
-# mode: julia 
-	 output = infer_ClaDS(my_tree, print_state = 100) 
-# mode: julia 
-	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDSe_100_7_4_clads_results.Rdata") 
-# mode: julia 
-	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDSe_100_7_5.tre") 
+	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDS2tree_100_6_3.tre") 
 # mode: julia 
 	 output = infer_ClaDS(my_tree, print_state = 100) 
 # mode: julia 
-	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDSe_100_7_5_clads_results.Rdata") 
+	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDS2tree_100_6_3_clads_results.Rdata") 
 # mode: julia 
-	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDSe_100_8_1.tre") 
-# mode: julia 
-	 output = infer_ClaDS(my_tree, print_state = 100) 
-# mode: julia 
-	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDSe_100_8_1_clads_results.Rdata") 
-# mode: julia 
-	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDSe_100_8_2.tre") 
+	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDS2tree_100_6_4.tre") 
 # mode: julia 
 	 output = infer_ClaDS(my_tree, print_state = 100) 
 # mode: julia 
-	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDSe_100_8_2_clads_results.Rdata") 
+	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDS2tree_100_6_4_clads_results.Rdata") 
 # mode: julia 
-	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDSe_100_8_3.tre") 
-# mode: julia 
-	 output = infer_ClaDS(my_tree, print_state = 100) 
-# mode: julia 
-	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDSe_100_8_3_clads_results.Rdata") 
-# mode: julia 
-	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDSe_100_8_4.tre") 
+	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDS2tree_100_6_5.tre") 
 # mode: julia 
 	 output = infer_ClaDS(my_tree, print_state = 100) 
 # mode: julia 
-	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDSe_100_8_4_clads_results.Rdata") 
+	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDS2tree_100_6_5_clads_results.Rdata") 
 # mode: julia 
-	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDSe_100_8_5.tre") 
+	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDS2tree_100_7_1.tre") 
 # mode: julia 
 	 output = infer_ClaDS(my_tree, print_state = 100) 
 # mode: julia 
-	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDSe_100_8_5_clads_results.Rdata") 
+	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDS2tree_100_7_1_clads_results.Rdata") 
+# mode: julia 
+	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDS2tree_100_7_2.tre") 
+# mode: julia 
+	 output = infer_ClaDS(my_tree, print_state = 100) 
+# mode: julia 
+	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDS2tree_100_7_2_clads_results.Rdata") 
+# mode: julia 
+	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDS2tree_100_7_3.tre") 
+# mode: julia 
+	 output = infer_ClaDS(my_tree, print_state = 100) 
+# mode: julia 
+	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDS2tree_100_7_3_clads_results.Rdata") 
+# mode: julia 
+	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDS2tree_100_7_4.tre") 
+# mode: julia 
+	 output = infer_ClaDS(my_tree, print_state = 100) 
+# mode: julia 
+	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDS2tree_100_7_4_clads_results.Rdata") 
+# mode: julia 
+	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDS2tree_100_7_5.tre") 
+# mode: julia 
+	 output = infer_ClaDS(my_tree, print_state = 100) 
+# mode: julia 
+	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDS2tree_100_7_5_clads_results.Rdata") 
+# mode: julia 
+	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDS2tree_100_8_1.tre") 
+# mode: julia 
+	 output = infer_ClaDS(my_tree, print_state = 100) 
+# mode: julia 
+	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDS2tree_100_8_1_clads_results.Rdata") 
+# mode: julia 
+	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDS2tree_100_8_2.tre") 
+# mode: julia 
+	 output = infer_ClaDS(my_tree, print_state = 100) 
+# mode: julia 
+	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDS2tree_100_8_2_clads_results.Rdata") 
+# mode: julia 
+	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDS2tree_100_8_3.tre") 
+# mode: julia 
+	 output = infer_ClaDS(my_tree, print_state = 100) 
+# mode: julia 
+	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDS2tree_100_8_3_clads_results.Rdata") 
+# mode: julia 
+	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDS2tree_100_8_4.tre") 
+# mode: julia 
+	 output = infer_ClaDS(my_tree, print_state = 100) 
+# mode: julia 
+	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDS2tree_100_8_4_clads_results.Rdata") 
+# mode: julia 
+	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/ClaDS2tree_100_8_5.tre") 
+# mode: julia 
+	 output = infer_ClaDS(my_tree, print_state = 100) 
+# mode: julia 
+	 save_ClaDS_in_R(output, "/home/tvasconcelos/missecomparison/ClaDScomparison/MiSSE_ClaDS_comparison/clads/ClaDS2tree_100_8_5_clads_results.Rdata") 
 # mode: julia 
 	 my_tree = load_tree("/home/tvasconcelos/missecomparison/ClaDScomparison/original_trees/trees/ClaDS2/NA.tre") 
 # mode: julia 
